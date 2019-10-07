b6984dfc65075e7ec4a8fbf5351ecccbeb8290a5
Ítalo <italo.ts4895@gmail.com>
2019-10-07 00:39:21 -0300

    Missing assets weren't added before cause I'm retarded

---
+++ Assets/Scripts/EnemyCoward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCoward : Enemy
+{
+    protected override void LookAround(){
+        Collider[] colliders = Physics.OverlapCapsule(transform.position, new Vector3(transform.position.x, 0, transform.position.z), detectionRange, LayerMask.GetMask("Slime"));
+        System.Array.Sort(colliders, CompareDistance);
+
+        if(colliders.Length > 0){
+            foreach(Collider col in colliders){
+                Slime slime = col.GetComponent<Slime>();
+                if(slime.CurrentState != Slime.SlimeState.Attacking
+                && slime.CurrentState != Slime.SlimeState.Flying
+                && slime.CurrentState != Slime.SlimeState.Null){
+                    Target = col.transform;
+                    SetState(EnemyState.Fleeing);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyReactive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyReactive : Enemy
+{
+    public new void Awake(){
+        base.Awake();
+        OnTakeDamage += Retaliate;
+    }
+
+    protected override void LookAround(){
+    }
+
+    protected void Retaliate(int dmg){
+        Debug.Log("chamou o callback");
+        Collider[] colliders = Physics.OverlapCapsule(transform.position, new Vector3(transform.position.x, 0, transform.position.z), detectionRange, LayerMask.GetMask("Slime"));
+        System.Array.Sort(colliders, CompareDistance);
+
+        if(colliders.Length > 0){
+            foreach(Collider col in colliders){
+                Slime slime = col.GetComponent<Slime>();
+                if(slime.CurrentState != Slime.SlimeState.Attacking
+                && slime.CurrentState != Slime.SlimeState.Flying
+                && slime.CurrentState != Slime.SlimeState.Null){
+                    Target = col.transform;
+                    SetState(EnemyState.Attacking);
+                }
+            }
+        }else{
+            Target = Slime.mainBody.transform;
+            SetState(EnemyState.Attacking);
+        }
+    }
+}
